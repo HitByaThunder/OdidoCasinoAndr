bec1267e129fe9b204122af42d9ff4f33595397b
133363 <jan@grappen.pl>
2024-01-12 12:17:28 +0100

    Spellen casino voor abonnees + logo en zwarte achtergrond [NEW]

---
+++ app/src/main/java/nl/odido/odidocasino/WachtwoordFragment.kt
@@ -0,0 +1,31 @@
+package nl.odido.odidocasino
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class WachtwoordFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = WachtwoordFragment()
+    }
+
+    private lateinit var viewModel: WachtwoordViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_spellen, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(WachtwoordViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/nl/odido/odidocasino/WachtwoordViewModel.kt
@@ -0,0 +1,7 @@
+package nl.odido.odidocasino
+
+import androidx.lifecycle.ViewModel
+
+class WachtwoordViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
