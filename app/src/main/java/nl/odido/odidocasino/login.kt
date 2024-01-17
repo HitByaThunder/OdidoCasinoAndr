bc529a91ad766839e1485dadbd00a88aeea7c2d5
henkie <henk@henkie.com>
2024-01-17 14:18:39 +0100

    maak acc 2

--- app/src/main/java/nl/odido/odidocasino/login.kt
+++ app/src/main/java/nl/odido/odidocasino/login.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -34,7 +36,12 @@ class login : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        val view = inflater.inflate(R.layout.fragment_login, container, false)
+        view.findViewById<Button>(R.id.buttonmakeacc).setOnClickListener {
+            //Use the navigation tree in the current view to navigate to the next page
+            Navigation.findNavController(view).navigate(R.id.lgsi)
+        }
+        return view
     }
 
     companion object {
@@ -56,4 +63,5 @@ class login : Fragment() {
                 }
             }
     }
+
 }
